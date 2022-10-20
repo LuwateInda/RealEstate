2451a57d64774d4fdc4b9d3163124013d52ec892
LuwateInda <luwate.inda@strathmore.edu>
2022-10-20 22:00:00 +0300

    Update conn.php
    DB settings update

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 define ('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','luwate');
+define('DB_PASS' ,'etawul');
 define('DB_NAME', 'myproperty');
 
 class DB_con{
